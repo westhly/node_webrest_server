f2dca86df49e6263d7f3a8936114dcea4dc20270
WesthlySarabia <westhly-carrillo@outlook.com>
2024-07-03 19:06:30 -0500

    para subir

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,24 @@
 import express from 'express'
+import path from 'path';
+
+interface Options {
+    port: number,
+    public_path?: string;
+}
 
 export class Server {
 
+    
+
     private app = express();
+    private readonly port: number;
+    private readonly publicPath: string
+
+    constructor(options:Options) {
+        const { port, public_path = 'public' } = options
+        this.port = port;
+        this.publicPath = public_path
+    }
 
 
     async start(){
@@ -10,9 +26,15 @@ export class Server {
         //*Middleware
 
         //*Public Folder
-        this.app.use(express.static('public'))
+        this.app.use(express.static(this.publicPath))
+
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`)
+            res.sendFile(indexPath)
+            
+        })
         
-        this.app.listen(3000, () => {
+        this.app.listen(this.port, () => {
             console.log(`server listening on port ${3000}`);
             
         })
